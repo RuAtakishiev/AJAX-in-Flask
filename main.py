9edb4ca89c03049240328ef84b4c773e14ac21ce
RuAtakishiev <atakishiev32ruslan@gmail.com>
2022-08-02 19:15:45 +0300

    Added AJAX GET request with button text and got data from the frontend

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    text = request.args.get('button_text')
+    print()
+    print('Button text:', text)
+    print()
     return render_template('index.html')
 
 if __name__ == '__main__':
