b29af126f03fda6d1b6ed698ae8885108ec59bcd
RuAtakishiev <atakishiev32ruslan@gmail.com>
2022-08-07 21:10:52 +0300

    Added an entry of the current time to the left list with a get request. And added a text entry from the left list to the right by clicking through a post request

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import json
+from time import time
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    text = request.args.get('button_text')
-    print()
-    print('Button text:', text)
-    print()
+    if request.is_json:
+
+        if request.method == 'GET':
+            seconds = time()
+            return jsonify({'seconds': seconds})
+        
+        if request.method == 'POST':
+            card_text = json.loads(request.data).get('text')
+            new_text = f'I got: {card_text}'
+            return jsonify({'data': new_text})
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -4,11 +4,23 @@ $(document).ready(function(){
             url: '',
             type: 'get',
             contentType: 'application/json',
-            data: {
-                button_text: $(this).text()
-            },
+            data: '',
             success: function(response){
                 $('.btn').text(response.seconds)
+                $('.left-list').append('<li>' + response.seconds + '</li>')
+            }
+        })
+    })
+    $('.left-list').on('click', 'li', function(){
+        $.ajax({
+            url: '',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                text: $(this).text()
+            }),
+            success: function(response){
+                $('.right-list').append('<li>' + response.data + '</li>')
             }
         })
     })
